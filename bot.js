d49705be28c0247d6a3e20accef3946d5809f14b
Dunaas <39232355+Dunaas@users.noreply.github.com>
2018-08-04 03:39:24 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,22 +12,22 @@ bot.on("messageCreate", (msg) => {
     var command = msg.content.toLowerCase();
     var args = msg.content.split(" ").slice(1);
 
-    if (command == '!evento' && eAndamento == false) {
-        bot.createMessage('300705416197701645', '``Evento iniciando! Use`` **!entrar** ``para participar!``')
+    if(command == '!evento' && eAndamento == false){
+        bot.createMessage('395994706477187083', '``Evento iniciando! Use`` **!entrar** ``para participar!``')
         eAndamento = true;
     }
-    if (command === "!entrar" && dEvent.length < 16 && dEvent.indexOf(msg.author.mention) == -1 && dEtapa == 0 && eAndamento == true) {
+    if(command === "!entrar" && dEvent.length < 16 && dEvent.indexOf(msg.author.mention) == -1 && dEtapa == 0 && eAndamento == true){
         msg.delete();
         dEvent.push(msg.author.mention);
         console.log(dEvent);
-        setTimeout(function() {
-            bot.createMessage('300705416197701645', msg.author.mention + " entrou!");
-        }, 5000);
+        setTimeout(function(){
+            bot.createMessage('395994706477187083', msg.author.mention + " entrou!");
+            }, 5000);
     }
-    if (dEvent.length == 16 && dEtapa == 0) {
+    if(dEvent.length == 16 && dEtapa == 0){
         dEtapa = 1;
         dEvent.push('noFlood');
-        bot.createMessage('300705416197701645', {
+        bot.createMessage('395994706477187083', {
             embed: {
                 title: "Evento X",
                 description: dEvent[0] + ' ⚔ ' + dEvent[1] + '\n' + dEvent[2] + ' ⚔ ' + dEvent[3] + '\n' + dEvent[4] + ' ⚔ ' + dEvent[5] + '\n' + dEvent[6] + ' ⚔ ' + dEvent[7] + '\n' + dEvent[8] + ' ⚔ ' + dEvent[9] + '\n' + dEvent[10] + ' ⚔ ' + dEvent[11] + '\n' + dEvent[12] + ' ⚔ ' + dEvent[13] + '\n' + dEvent[14] + ' ⚔ ' + dEvent[15],
@@ -44,14 +44,14 @@ bot.on("messageCreate", (msg) => {
         console.log(dEvent);
         dEvent = [];
     }
-    if (command.indexOf("!classificar") != -1 && args[0] != null && dEvent.indexOf(args[0]) == -1 && args[0].length == 21 && dEtapa == 1 && dStaffOrg.indexOf(msg.author.id) != -1) {
+    if(command.indexOf("!classificar") != -1 && args[0] != null && dEvent.indexOf(args[0]) == -1 && args[0].length == 21 && dEtapa == 1 && dStaffOrg.indexOf(msg.author.id) != -1){
         msg.delete();
         dEvent.push(args[0]);
         console.log(dEvent);
     }
-    if (dEvent.length == 8 && dEtapa == 1) {
+    if(dEvent.length == 8 && dEtapa == 1){
         dEvent.push('noFlood');
-        bot.createMessage('300705416197701645', {
+        bot.createMessage('395994706477187083',{
             embed: {
                 title: "Evento X",
                 description: dEvent[0] + ' ⚔ ' + dEvent[1] + '\n' + dEvent[2] + ' ⚔ ' + dEvent[3] + '\n' + dEvent[4] + ' ⚔ ' + dEvent[5] + '\n' + dEvent[6] + ' ⚔ ' + dEvent[7],
@@ -67,18 +67,18 @@ bot.on("messageCreate", (msg) => {
         });
         console.log(dEvent);
         dEvent = [];
-        setTimeout(function() {
-            dEtapa = 2;
+        setTimeout(function(){
+        dEtapa = 2;
         }, 100);
     }
-    if (command.indexOf("!classificar") != -1 && args[0] != null && dEvent.indexOf(args[0]) == -1 && args[0].length == 21 && dEtapa == 2 && dStaffOrg.indexOf(msg.author.id) != -1) {
+    if(command.indexOf("!classificar") != -1 && args[0] != null && dEvent.indexOf(args[0]) == -1 && args[0].length == 21 && dEtapa == 2 && dStaffOrg.indexOf(msg.author.id) != -1){
         msg.delete();
         dEvent.push(args[0]);
         console.log(dEvent);
     }
-    if (dEvent.length == 4 && dEtapa == 2) {
+    if(dEvent.length == 4 && dEtapa == 2){
         dEvent.push('noFlood');
-        bot.createMessage('300705416197701645', {
+        bot.createMessage('395994706477187083',{
             embed: {
                 title: "Evento X",
                 description: dEvent[0] + ' ⚔ ' + dEvent[1] + '\n' + dEvent[2] + ' ⚔ ' + dEvent[3],
@@ -94,18 +94,18 @@ bot.on("messageCreate", (msg) => {
         });
         console.log(dEvent);
         dEvent = [];
-        setTimeout(function() {
-            dEtapa = 3;
-        }, 100);
+        setTimeout(function(){
+        dEtapa = 3;
+     }, 100);
     }
-    if (command.indexOf("!classificar") != -1 && args[0] != null && dEvent.indexOf(args[0]) == -1 && args[0].length == 21 && dEtapa == 3 && dStaffOrg.indexOf(msg.author.id) != -1) {
+    if(command.indexOf("!classificar") != -1 && args[0] != null && dEvent.indexOf(args[0]) == -1 && args[0].length == 21 && dEtapa == 3 && dStaffOrg.indexOf(msg.author.id) != -1){
         msg.delete();
         dEvent.push(args[0]);
         console.log(dEvent);
     }
-    if (dEvent.length == 2 && dEtapa == 3) {
+    if(dEvent.length == 2 && dEtapa == 3){
         dEvent.push('noFlood');
-        bot.createMessage('300705416197701645', {
+        bot.createMessage('395994706477187083',{
             embed: {
                 title: "Evento X",
                 description: dEvent[0] + ' ⚔ ' + dEvent[1],
@@ -121,15 +121,15 @@ bot.on("messageCreate", (msg) => {
         });
         console.log(dEvent);
         dEvent = [];
-        setTimeout(function() {
-            dEtapa = 4;
-        }, 100);
+        setTimeout(function(){
+        dEtapa = 4;
+     }, 100);
     }
-    if (command.indexOf("!ganhador") !== -1 && args[0] != null && dEtapa == 4 && dStaffOrg.indexOf(msg.author.id) != -1) {
+    if (command.indexOf("!ganhador") !== -1 && args[0] != null && dEtapa == 4 && dStaffOrg.indexOf(msg.author.id) != -1){
         msg.delete();
-        bot.createMessage('300705416197701645', args[0] + ' **GANHOU!!**')
-        setTimeout(function() {
-            dEtapa = 0;
+        bot.createMessage('395994706477187083', args[0] + ' **GANHOU!!**')
+        setTimeout(function(){
+        dEtapa = 0;
         }, 100);
         dEvent = [];
     }
