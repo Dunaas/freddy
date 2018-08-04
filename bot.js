c102e87edc35877b24955dc2674188347b5e9307
Dunaas <39232355+Dunaas@users.noreply.github.com>
2018-08-04 18:45:18 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ var Eris = require('eris');
 var bot = new Eris(process.env.BOT_TOKEN);
 var dEvent = [];
 var eAndamento = false;
-var dEtapa = 1;
+var dEtapa = 0;
 var dStaffOrg = ['211962239433834498', '288021215535300608'];
 bot.on("ready", () => {
     console.log("Ready!");
